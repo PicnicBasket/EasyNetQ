2fcf66525e32165a5d32674300c105a280efc934
Andrew Browne <andrew@adcastgroup.com>
2012-05-22 16:09:03 +1000

    Setup to build internal package

--- Source/EasyNetQ.Scheduler/Program.cs
+++ Source/EasyNetQ.Scheduler/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using log4net.Config;
-using Topshelf;
+//using Topshelf;
 
 namespace EasyNetQ.Scheduler
 {
@@ -11,25 +11,25 @@ namespace EasyNetQ.Scheduler
         {
             XmlConfigurator.Configure();
 
-            HostFactory.Run(hostConfiguration =>
-            {
-                hostConfiguration.AfterStartingServices(() => Console.WriteLine("Started EasyNetQ.Scheduler"));
-                hostConfiguration.AfterStoppingServices(() => Console.WriteLine("Stopped EasyNetQ.Scheduler"));
-                // hostConfiguration.EnableDashboard();
-                hostConfiguration.RunAsLocalSystem();
-                hostConfiguration.SetDescription("EasyNetQ.Scheduler");
-                hostConfiguration.SetDisplayName("EasyNetQ.Scheduler");
-                hostConfiguration.SetServiceName("EasyNetQ.Scheduler");
+            //HostFactory.Run(hostConfiguration =>
+            //{
+            //    hostConfiguration.AfterStartingServices(() => Console.WriteLine("Started EasyNetQ.Scheduler"));
+            //    hostConfiguration.AfterStoppingServices(() => Console.WriteLine("Stopped EasyNetQ.Scheduler"));
+            //    // hostConfiguration.EnableDashboard();
+            //    hostConfiguration.RunAsLocalSystem();
+            //    hostConfiguration.SetDescription("EasyNetQ.Scheduler");
+            //    hostConfiguration.SetDisplayName("EasyNetQ.Scheduler");
+            //    hostConfiguration.SetServiceName("EasyNetQ.Scheduler");
 
-                hostConfiguration.Service<ISchedulerService>(serviceConfiguration =>
-                {
-                    serviceConfiguration.SetServiceName("SchedulerService");
-                    serviceConfiguration.ConstructUsing(_ => SchedulerServiceFactory.CreateScheduler());
+            //    hostConfiguration.Service<ISchedulerService>(serviceConfiguration =>
+            //    {
+            //        serviceConfiguration.SetServiceName("SchedulerService");
+            //        serviceConfiguration.ConstructUsing(_ => SchedulerServiceFactory.CreateScheduler());
 
-                    serviceConfiguration.WhenStarted(service => service.Start());
-                    serviceConfiguration.WhenStopped(service => service.Stop());
-                });
-            });
+            //        serviceConfiguration.WhenStarted(service => service.Start());
+            //        serviceConfiguration.WhenStopped(service => service.Stop());
+            //    });
+            //});
         }
     }
 }
