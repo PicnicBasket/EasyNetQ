2070e5a4af1ff56c8ac1a1041d6342b6b1835811
Andrew Browne <brownie@brownie.com.au>
2012-05-29 10:49:22 +1000

    Json Serialization settings
    Add Json serialization settings to allows collections of interfaces to be serialized

--- Source/EasyNetQ/JsonSerializer.cs
+++ Source/EasyNetQ/JsonSerializer.cs
@@ -5,14 +5,20 @@ namespace EasyNetQ
 {
     public class JsonSerializer : ISerializer
     {
+        readonly JsonSerializerSettings settings = new JsonSerializerSettings()
+        {
+            TypeNameHandling = TypeNameHandling.Auto,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        };
+
         public byte[] MessageToBytes<T>(T message)
         {
-            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message, settings));
         }
 
         public T BytesToMessage<T>(byte[] bytes)
         {
-            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(bytes));
+            return JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(bytes), settings);
         }
     }
 }
